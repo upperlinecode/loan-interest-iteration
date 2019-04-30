a1528fcfbe1cafc31f7154f3bb1172060a16a577
Jeffrey Lancaster <jeffrey.lancaster@gmail.com>
2019-04-30 20:26:06 +0000

    add loan.py

---
+++ loan.py
@@ -0,0 +1,49 @@
+### Check the README for additional information ###
+
+# 1. Create a function called simple_payment that only accepts monthly payment and returns the number of months it will take to repay a $3,000 zero-interest, zero-fee loan.
+
+
+
+# 2. Create a function called repay_low_interest that takes in a loan amount and a monthly payment. It should integrate the bank's fee and the interest rate to return the number of months it would take to repay that loan.
+
+
+
+# Bonus: if the loan amount is outside the bank's acceptable loan range, suggest the user Borrow a different amount.
+
+
+
+# 3. Create a more flexible fare calculator method called repay_calculator that takes in the loan amount, monthly payment, and loan type and returns the number of months it would rake to repay that loan.
+
+
+
+# Bonus: if no loan type is given, have repay_calculator default to Medium-Interest.
+
+
+
+# Double-Bonus: if the loan amount is outside the bank's acceptable loan range, suggest the user Borrow a different amount.
+
+
+
+### Advanced Bonus Challenge ###
+
+# 4. Which loan type is best? Create a method best_loan that accepts the loan amount and monthly payment, and returns which loan type will cost the borrower the least along with the cost to the borrower to take that type of loan. This is an open ended one - there are many ways to do this!
+
+
+
+# Bonus: is "EZ Loan" ever the best kind of loan? Under what circumstances is it better than the other options?
+
+
+
+### Double Advanced Bonus Challenge ###
+
+# 5. What if instead of assessing interest at the beginning of the loan period, the interest is (more-realistically) assessed monthly? Create a new repay method that adds interest at the end of each month. Assume good behavior by the loan recipient who pays back the expected amount before the interest is assessed on the remaining balance.
+
+
+
+# Bonus: How does repay (assessing monthly interest) compare to repay_calculator (assessing one-time interest)?
+
+
+
+# Double-Bonus: Use the same idea of assessing monthly interest to write a new best method that returns which loan type will cost the borrower the least (including monthly interest and bank fees) along with the cost to the borrower to take that type of loan.
+
+
